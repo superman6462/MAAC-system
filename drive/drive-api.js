8000e7fbb987ae0ce05a69dde5d0bf58286d212a
superman6462 <easniton@gmail.com>
2026-06-30 19:48:12 +0600

    Update DRIVE_FOLDER_ID for Google Drive API

--- drive/drive-api.js
+++ drive/drive-api.js
@@ -1,4 +1,4 @@
-const DRIVE_FOLDER_ID = "17TkAb01lkQLseNzVpWkpUm-L-ZZkgRe-";
+const DRIVE_FOLDER_ID = "1CuDyA7uqjqobXyoK9xE8zPEihXIyWeGs";
 // For serverless Drive upload, we can use the Google Drive API directly from the browser with an API key (limited).
 // For full OAuth, we'd need a backend, but we can still create shareable links via manual upload.
 // This stub is for reference. Actual file upload is done by manager via Firebase Storage or manual Drive.
